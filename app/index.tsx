71b73871fef47ccbe2ff8865e45cc2fb1219748c
Черногривов Кирилл <kirill.2201@inbox.ru>
2024-08-22 21:05:06 +0700

    fresh

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,5 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import {Redirect} from 'expo-router'
 
-export default function Page() {
-    const { user } = useUser()
-
-    return (
-        <View>
-            <SignedIn>
-                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-            </SignedIn>
-            <SignedOut>
-                <Link href="/sign-in">
-                    <Text>Sign In</Text>
-                </Link>
-                <Link href="/sign-up">
-                    <Text>Sign Up</Text>
-                </Link>
-            </SignedOut>
-        </View>
-    )
+export default function index() {
+    return <Redirect href={"/home"} />
 }
