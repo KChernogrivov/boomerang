21fa1195747e9e5444edc713ef632e5b3998ec3f
Черногривов Кирилл <kirill.2201@inbox.ru>
2024-08-22 22:18:40 +0700

    search bar

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,16 @@
-import {View, Text} from "react-native";
+import {StyleSheet, View} from "react-native";
+import HeaderHome from "@/components/home/header-home";
 
 export default function home() {
     return (
-        <View>
-            <Text>Home page</Text>
+        <View style={styles.home}>
+            <HeaderHome />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    home: {
+        flex: 1,
+    }
+})
---
+++ components/home/header-home.tsx
@@ -0,0 +1,59 @@
+import {View, StyleSheet, Text, TextInput, Pressable} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+import React from "react";
+
+export default function headerHome() {
+    const [value, onChangeText] = React.useState('');
+
+    return (
+        <View style={styles.header}>
+            <View style={styles.search}>
+                <View style={styles.searchBar}>
+                    <Ionicons style={styles.searchBarIcon} name="search" size={18}/>
+                    <TextInput style={styles.searchBarInput} inputMode='search' placeholder="Поиск"
+                               onChangeText={text => onChangeText(text)}
+                               value={value}
+                    />
+                </View>
+                <Pressable onPress={() => {
+                    onChangeText('')
+                }}>
+                    <Text style={styles.searchClear}>Очистить</Text>
+                </Pressable>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: 50,
+        padding: 10,
+        backgroundColor: 'black',
+    },
+    search: {
+        flexDirection: "row",
+        alignItems: 'center',
+        gap: 10,
+    },
+    searchBar: {
+        flex: 1,
+        flexDirection: "row",
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    searchBarIcon: {
+        marginRight: 10,
+    },
+    searchBarInput: {
+        flex: 1,
+        padding: 5,
+    },
+    searchClear: {
+        color: '#fff',
+    }
+})
