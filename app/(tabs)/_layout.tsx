cb802eb4a5d225c0ab9f205ef5d66d28c410212c
Черногривов Кирилл <kirill.2201@inbox.ru>
2024-08-22 21:01:13 +0700

    fresh

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,23 @@
 import {Tabs} from "expo-router";
-import {FontAwesome, Ionicons} from "@expo/vector-icons";
+import {Ionicons} from "@expo/vector-icons";
 
-export default function TabsLayout() {
+export default function _layout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="post" options={{
-                headerTitle: 'Разместить',
-                title: 'Разместить',
-                tabBarIcon: ({color, focused}) => <Ionicons size={28}
-                                                            name={focused ? 'add-circle' : 'add-circle-outline'}
-                                                            color={color}/>,
-            }}/>
-            <Tabs.Screen name="index" options={{
-                headerTitle: 'Главная',
-                title: 'Главная',
-                tabBarIcon: ({color, focused}) => <Ionicons size={28} name={focused ? 'home' : 'home-outline'}
-                                                            color={color}/>,
-            }}/>
-            <Tabs.Screen name="user/index" options={{
-                headerTitle: 'Профиль',
-                title: 'Профиль',
-                tabBarIcon: ({color, focused}) =>
-                    <FontAwesome size={28} name={focused ? 'user' : 'user-o'} color={color}/>,
-            }}/>
+        <Tabs screenOptions={{
+            headerShown: false,
+        }}>
+            <Tabs.Screen name="create-post" options={{
+                tabBarLabel: "Разместить",
+                tabBarIcon: ({color}) => <Ionicons name="add-circle" size={24} color={color}/>,
+            }} />
+            <Tabs.Screen name="home" options={{
+                tabBarLabel: "Главная",
+                tabBarIcon: ({color}) => <Ionicons name="home" size={24} color={color}/>,
+            }} />
+            <Tabs.Screen name="profile" options={{
+                tabBarLabel: "Профиль",
+                tabBarIcon: ({color}) => <Ionicons name="person-circle" size={24} color={color}/>,
+            }} />
         </Tabs>
     )
 }
---
+++ app/(tabs)/create-post.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function createPost() {
+    return (
+        <View>
+            <Text>createPost page</Text>
+        </View>
+    )
+}
